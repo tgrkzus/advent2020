4df7c10e3849bf61dd346d39db68dc2477a14aa3
Tom Richardson <tgrkzus@gmail.com>
2020-12-01 20:20:24 +1100

    lol solved

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,28 @@
+use super::*;
 
-pub(crate) fn run(input: &Vec<String>) {
-    dbg!("Hello");
+pub fn run(input: &Vec<String>) {
+    first(input);
+    second(input);
+}
+
+fn first(input: &Vec<String>) {
+    let result = input.iter()
+        .map(|v| v.parse::<u32>().unwrap())
+        .combinations(2)
+        .filter(|comb| comb[0] + comb[1] == 2020)
+        .last()
+        .map(|comb| comb[0] * comb[1])
+        .unwrap();
+    println!("{}", result);
+}
+
+fn second(input: &Vec<String>) {
+    let result = input.iter()
+        .map(|v| v.parse::<u32>().unwrap())
+        .combinations(3)
+        .filter(|comb| comb[0] + comb[1] + comb[2] == 2020)
+        .last()
+        .map(|comb| comb[0] * comb[1] * comb[2])
+        .unwrap();
+    println!("{}", result);
 }
